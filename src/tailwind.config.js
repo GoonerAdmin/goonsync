4250193c5bee00e840773f397544f5453bd5ec67
GoonerAdmin <admin@goonsync.com>
2025-12-01 10:43:02 +1100

    Fixed Tailwind config for Create React App

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -6,17 +6,12 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        // X (Twitter) inspired colors
         'x-blue': '#1D9BF0',
         'x-blue-hover': '#1A8CD8',
         'x-gray': '#536471',
         'x-border': '#2F3336',
         'x-hover': '#16181C',
       },
-      borderRadius: {
-        'xl': '16px',
-        '2xl': '20px',
-      }
     },
   },
   plugins: [],
